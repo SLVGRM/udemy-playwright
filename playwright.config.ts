68c17bd0036a78216d8d101b43ae5e47360265da
lvl <slava.larionov@gmail.com>
2024-01-23 23:24:20 +0500

    added screenshots on failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,14 +2,14 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
     timeout: 10000,
-    retries: 2,
+    retries: 0,
     use: {
-        headless: false, 
+        headless: true, 
         viewport: {width: 1280, height: 720},
         actionTimeout: 15000,
         ignoreHTTPSErrors: true,
         video: 'off',
-        screenshot: 'off'
+        screenshot: 'only-on-failure'
     }
 }
 
